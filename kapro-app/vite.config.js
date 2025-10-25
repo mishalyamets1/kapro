16f37ff90ab22241935efcaca294f81c09205711
mishalyamets <mishalyamets56@gmail.com>
2025-10-25 19:19:22 +0300

    adasdasdasd

--- kapro-app/vite.config.js
+++ kapro-app/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root:'.',
+  base: process.env.NODE_ENV === 'production' ? '/kapro/' : '/', 
   build: { outDir: 'dist' }
 })
